27c74a0459b0fc9657922f6ccaa951a35b74e3a2
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-16 15:02:49 -0300

    Update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -153,7 +153,12 @@ class CreditsState extends MusicBeatState
 		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
-		super.create();
+		
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end	
+		
+			super.create();
 	}
 
 	override function update(elapsed:Float)
