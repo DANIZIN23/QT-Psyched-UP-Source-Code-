6f1a633d6cd605b32c2ac387a22e4c78d621874e
DANIZIN <98532797+DANIZIN23@users.noreply.github.com>
2022-10-16 15:06:25 -0300

    Update Character.hx

--- source/Character.hx
+++ source/Character.hx
@@ -98,11 +98,11 @@ class Character extends FlxSprite
 
 			default:
 				var characterPath:String = 'characters/' + curCharacter + '.json';
-				#if MODS_ALLOWED
 				
 				
 				
 				
+				#if windows 
 				var path:String = Paths.modFolders(characterPath);
 				if (!FileSystem.exists(path)) {
 					path = Paths.getPreloadPath(characterPath);
@@ -120,7 +120,7 @@ class Character extends FlxSprite
 					path = Paths.getPreloadPath('characters/' + DEFAULT_CHARACTER + '.json'); //If a character couldn't be found, change him to BF just to prevent a crash
 				}
 
-				#if MODS_ALLOWED
+				#if windows 
 				var rawJson = File.getContent(path);
 				#else
 				var rawJson = Assets.getText(path);
@@ -151,7 +151,7 @@ class Character extends FlxSprite
 				
 				
 				
-				#if MODS_ALLOWED
+				#if windows 
 				var modAnimToFind:String = Paths.modFolders('images/' + json.image + '/Animation.json');
 				var animToFind:String = Paths.getPath('images/' + json.image + '/Animation.json', TEXT);
 				
